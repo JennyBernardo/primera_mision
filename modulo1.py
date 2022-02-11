cc1050898d9a6e23478971578010cba62b026610
Jenny Bernardo <vasti_2011@hotmail.com>
2022-02-10 21:44:02 -0600

    first commit

---
+++ modulo1.py
@@ -0,0 +1,8 @@
+print("Bienvenido al programa de bienvenida")
+name = input("Introduzca su nombre ")
+print("Saludos: " + name)
+
+print("Calculadora")
+first_number = input("Primer número: ")
+second_number = input("Segundo número: ")
+print(int(first_number) + int(second_number))
